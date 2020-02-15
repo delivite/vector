977d1fd7aa3b030c8012125cb3c7169f2cafcff9
delivite <mrwhistler316@gmail.com>
2020-02-15 15:25:20 +0100

    Refactored members functions

--- bucket.h
+++ bucket.h
@@ -9,84 +9,123 @@
 #define BUCKET_H_
 
 #include<iostream>
-#include<cstdlib>
+#include<typeinfo>
 #include<initializer_list>
+#include <exception>
 
 namespace my {
 
-template<typename T>
+template <typename T>
 class bucket {
-	int m_size { 0 };
-	T *m_list = new T[10];
-	int m_position { 0 };
-	int m_capacity { 10 };
-
-private:
-	void new_array(int new_size) {
-		//Expands the capacity of the vector
-		T temp[m_size];
-		for (int i = 0; i < m_size; i++) {
-			temp[i] = m_list[i];
-		}
-
-		delete[] m_list;
-
-		m_list = new T[new_size]; //Re-allocates the pointer.
-
-		for (int i = 0; i < m_size; i++) {
-			m_list[i] = temp[i];
-		}
-		m_capacity = new_size;
-	}
-
 public:
 	class iterator;
 
 public:
 
-	bucket() = default; //Default constructor
-	bucket(const bucket &other) = default;
-	bucket& operator=(const bucket &other) = default;
-
-	bucket(int size) {
-		//Parameterized constructor to create a vector of size elements.
-		m_list = new T[size];
-		m_size = size;
-
-		m_capacity = m_size;
+	//Default Constructor
+	bucket() {
+		std::cout << "Constructor" << std::endl;
 	}
 
-	bucket(int size, int init) {
-		//Parameterized constructor - Creates size array and initialize the elements with init.
-		m_list = new T[size];
-		for (T i = 0; i < size; i++) {
-			m_list[i] = init;
+	//Parameterized constructor with all elements initialized to 0
+	//To avoid conversion from argument type to class, this constructor is marked explicit
+	explicit bucket(const int size) :
+			m_size(size), m_pArray(new T[size] { }), m_capacity(m_size) {
+
+	}
+
+	//Parameterized constructor with all elements initialized to second argument
+	bucket(const int size, const T &init) :
+			m_size(size), m_pArray(new T[size] { }), m_capacity(m_size) {
+
+		for (int i = 0; i < size; i++) {
+			m_pArray[i] = init;
 		}
-		m_size = size;
-		m_capacity = m_size;
 	}
 
 
+	//Constructor with initializer list
 	bucket(std::initializer_list<T> list) {
 		for (T value : list) {
 			add(value);
 		}
 	}
 
+	//Copy Constructor
+	bucket(const bucket &other) :
+			m_size(other.m_size), m_position(other.m_position), m_capacity(
+					other.m_capacity) {
+		for (int i = 0; i < m_size; i++) {
+			m_pArray[i] = other.m_pArray[i];
+		}
+	}
 
+	//Copy Assignment Constructor
+	bucket& operator=(const bucket &orig) {
+		if (this == &orig) {
+			return *this;
+		}
+
+		if (orig.m_size <= m_capacity) { //Just copy the elements if size is less than allocated capacity.
+			//Memory allocation not needed.
+			for (int i = 0; i < orig.m_size; i++) {
+				m_pArray[i] = orig.m_pArray[i];
+			}
+			m_size = orig.m_size;
+			m_position = orig.m_position;
+			m_capacity = orig.m_capacity;
+
+			return *this;
+		}
+
+		T *list = new T[orig.m_size];//Allocate memory on the heap if we are out of reserved space
+		for (int i = 0; i < orig.m_size; i++) {
+			list[i] = orig.m_pArray[i];
+		}
+
+		delete[] m_pArray;
+		m_pArray = list;
+
+		m_size = orig.m_size;
+		m_position = orig.m_position;
+		m_capacity = orig.m_capacity;
+		return *this;
+	}
+
+	//Move constructor
+	bucket(bucket &&orig) :
+			m_size(orig.m_size), m_pArray(orig.m_pArray), m_position(
+					orig.m_position), m_capacity(orig.m_capacity) {
+		orig.m_pArray = nullptr;
+		orig.m_size = 0;
+		orig.m_capacity = 0;
+		orig.m_position = 0;
+	}
+
+	//Move Assignment Constructor
+	bucket& operator=(bucket &&orig) {
+		delete[] m_pArray;
+		m_pArray = orig.m_pArray;
+		m_size = orig.m_size;
+		m_capacity = orig.m_capacity;
+		m_position = orig.m_position;
+
+		orig.m_pArray = nullptr;
+		orig.m_size = 0;
+		orig.m_capacity = 0;
+		orig.m_position = 0;
+
+		return *this;
+	}
+
+	//Destructor
 	~bucket() {
-		delete[] m_list;
+		delete[] m_pArray;
 	}
 
-
-	void pop_last() {
-		m_size--;
-		m_position--;
-	}
-
-
-	void add(T value) {
-		m_list[m_position] = value;
+	//Equivalent to std::vector<T>::push_back()
+	void add_element(const T &value) {
+		m_pArray[m_position] = value;
 		m_position++;
 
 		if (m_position > m_size) {
@@ -94,75 +133,128 @@ public:
 		}
 
 		if (m_position >= m_capacity) {
-			new_array(m_capacity + 8);
+			resize_vector(m_capacity * 2);
 		}
 	}
 
 
-	void insert(std::initializer_list<T> list) {
+	void insert_element(std::initializer_list<T> list) {
 		for (T x : list) {
 			add(x);
 		}
 	}
 
+	T& at(int index) {
+		if (!is_in_range(index)) {
+			throw std::out_of_range("Error: Index is out 00range");
+		} else
+			return m_pArray[index];
+	}
 
-	int capacity() {
+	const T& at(int inde) const {
+		if (!is_in_range(inde)) {
+			throw std::out_of_range("Error: Index is out Weird");
+		} else
+			return m_pArray[inde];
+	}
+
+	const T& operator[](int index) const {
+		return m_pArray[index];
+	}
+
+	T& operator[](int index) {
+		return m_pArray[index];
+	}
+
+	void pop_last() {
+		m_size--;
+		m_position--;
+	}
+
+	void resize(const int new_size) {
+		if (new_size < m_capacity || new_size < 0) {
+			return;
+		} else
+			resize_vector(new_size);
+	}
+
+	int capacity() const {
 		return m_capacity;
 	}
 
-
-	int size() {
+	int size() const {
 		return m_size;
 	}
-
-
-	T& at(int position) {
-		return m_list[position];
-	}
-
-
-	std::ostream& operator<<(std::ostream &out) const {
-		out << m_list[m_position];
-		return out;
-	}
-
-
-	T& operator[](int index) const {
-		return m_list[index];
-	}
-
 
 	iterator begin() {
 		return iterator(0, *this);
 	}
-
 
 	iterator end() {
 		return iterator(m_size, *this);
 	}
 
 
-	void reverse() {
-		//Reverse the elements of the array
-		T *begin = m_list;
-		T *end = &m_list[m_size - 1];
+private: //Private member variables
+	int m_size { 0 };
+	T *m_pArray = new T[10] { }; //Allocate an array of free memory and initialize the elements to 0
+	int m_position { 0 };
+	int m_capacity { 10 };
 
-		while (begin < end) {
-
-			T temp = *begin;
-			*begin = *end;
-			*end = temp;
-
-			begin++;
-			end--;
+private: //Private member functions
+	//Resize the vector
+	void resize_vector(const int new_size, T def = T()) {
+		std::cout << "Let's create a new vector" << std::endl;
+		T temp[m_size];
+		for (int i = 0; i < m_size; i++) {
+			temp[i] = m_pArray[i];
 		}
+
+		delete[] m_pArray;
+
+		m_pArray = new T[new_size];
+
+		for (int i = 0; i < m_size; i++) {
+			m_pArray[i] = temp[i];
+		}
+		m_capacity = new_size;
+	}
+
+	//Range checking function
+	bool is_in_range(const int index) {
+		if (index < 0 || index >= m_size) {
+			return false;
+		}
+		return true;
 	}
 };
 
+template<typename T>
+void reverse(bucket<T> &orig) {
+	//Reverse the elements of the array
+	auto begin = orig.begin();
+	auto end = --orig.end();	//Move the iterator 1 place back to keep within range
+
+	std::cout << typeid(begin).name()<< std::endl;
+
+	while (begin < end) {
+
+		std::swap(*begin, *end);
+
+		begin++;
+		end--;
+	}
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream &out, const bucket<T> &orig) {
+	out << orig.m_pArray[orig.m_position];
+	return out;
+}
 
 template<typename T>
 class bucket<T>::iterator {
-	int m_index;
+	int m_index{0};
 	bucket &m_bucket;
 
 public:
@@ -170,18 +262,20 @@ public:
 			m_index(pos), m_bucket(container) {
 	}
 
+	iterator& operator=(iterator &other) {
+		m_bucket[m_index] = other.m_bucket[m_index];
+		return *this;
+	}
 
-	T& operator*() {
+	T& operator*() const {
 
 		return m_bucket.at(m_index);
 	}
-
 
 	iterator& operator++() {
 		m_index++;
 		return *this;
 	}
-
 
 	iterator& operator++(int) {
 		++m_index;
@@ -202,8 +296,16 @@ public:
 		return m_index != copy.m_index;
 	}
 
-	bool const& operator<(const iterator &other) const {
+	bool /*const&*/ operator<(const iterator &other) const {
 		return (m_index < other.m_index);
+	}
+
+	bool /*const&*/ operator<=(const iterator &other) const {
+		return (m_index <= other.m_index);
+	}
+
+	bool /*const&*/ operator>(const iterator &other) const {
+		return (m_index > other.m_index);
 	}
 };
 
